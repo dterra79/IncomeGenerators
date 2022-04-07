cee0a74a8443922803ed66e03f9fcf21fbdcec1a
dterra79 <98139891+dterra79@users.noreply.github.com>
2022-04-07 13:25:23 -0600

    Added Java Snake Game

---
+++ snakegame/src/snakegame/snakemain.java
@@ -0,0 +1,13 @@
+package snakegame;
+
+//shout out to Bro Code on YouTube for explaining most of these 3 classes
+
+public class snakemain 
+{
+
+	public static void main(String[] args) 
+	{
+		GameFrame frame = new GameFrame();
+	}
+
+}
